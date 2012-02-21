5503382729d2f87cf30bfc2e84ca9e1d2403967a
Jörn Horstmann <git@jhorstmann.net>
2012-02-21 22:47:21 +0100

    Some more tests in the sample webapp

--- i18n-sample-webapp/src/test/java/net/jhorstmann/i18n/sample/TranslationTest.java
+++ i18n-sample-webapp/src/test/java/net/jhorstmann/i18n/sample/TranslationTest.java
@@ -2,13 +2,26 @@ package net.jhorstmann.i18n.sample;
 
 import java.util.Locale;
 import net.jhorstmann.i18n.I18N;
+import org.junit.After;
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
 /**
  * Translation works in Tests by configuring the neccessary Providers in src/test/resources.
  */
 public class TranslationTest {
+    private Locale locale;
+    @Before
+    public void saveLocale() {
+        locale = Locale.getDefault();
+    }
+
+    @After
+    public void restoreLocale() {
+        Locale.setDefault(locale);
+    }
+
     @Test
     public void testGerman() {
         Locale.setDefault(Locale.GERMANY);
@@ -17,9 +30,41 @@ public class TranslationTest {
     }
 
     @Test
+    public void testGermanPlural() {
+        Locale.setDefault(Locale.GERMANY);
+        Assert.assertEquals("Mehrere Nachrichten", I18N.trn("One Message", "Several Messages", 0));
+        Assert.assertEquals("Eine Nachricht", I18N.trn("One Message", "Several Messages", 1));
+        Assert.assertEquals("Mehrere Nachrichten", I18N.trn("One Message", "Several Messages", 2));
+    }
+
+    @Test
     public void testEnglish() {
         Locale.setDefault(Locale.ENGLISH);
         Assert.assertEquals("Simple message", I18N.tr("Simple message"));
         Assert.assertEquals("Message with context", I18N.trc("Message with context", "Message with context"));
     }
+
+    @Test
+    public void testEnglishPlural() {
+        Locale.setDefault(Locale.ENGLISH);
+        Assert.assertEquals("Several Messages", I18N.trn("One Message", "Several Messages", 0));
+        Assert.assertEquals("One Message", I18N.trn("One Message", "Several Messages", 1));
+        Assert.assertEquals("Several Messages", I18N.trn("One Message", "Several Messages", 2));
+    }
+
+    @Test
+    public void testDefault() {
+        Locale.setDefault(Locale.CHINESE);
+        Assert.assertEquals("Simple message", I18N.tr("Simple message"));
+        Assert.assertEquals("Message with context", I18N.trc("Message with context", "Message with context"));
+    }
+
+    @Test
+    public void testDefaultPlural() {
+        Locale.setDefault(Locale.CHINESE);
+        Assert.assertEquals("Several Messages", I18N.trn("One Message", "Several Messages", 0));
+        Assert.assertEquals("One Message", I18N.trn("One Message", "Several Messages", 1));
+        Assert.assertEquals("Several Messages", I18N.trn("One Message", "Several Messages", 2));
+    }
+
 }
