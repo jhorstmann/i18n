fef31b0c6f5ad23a75cf24ebe3ac62f11d4a3a27
Jörn Horstmann <git@jhorstmann.net>
2012-03-29 13:22:33 +0200

    Some tests for number formatting with MessageFormat

---
+++ i18n-core/src/test/java/net/jhorstmann/i18n/MessageFormatTest.java
@@ -0,0 +1,29 @@
+package net.jhorstmann.i18n;
+
+import java.math.BigDecimal;
+import java.text.MessageFormat;
+import java.util.Locale;
+import org.junit.Assert;
+import org.junit.Test;
+
+public class MessageFormatTest {
+    @Test
+    public void testFormatInteger() {
+        Assert.assertEquals("1.234", new MessageFormat("{0,number,integer}", Locale.GERMANY).format(new Object[]{Integer.valueOf(1234)}));
+    }
+
+    @Test
+    public void testFormatIntegerNoGrouping() {
+        Assert.assertEquals("1234", new MessageFormat("{0,number,#}", Locale.GERMANY).format(new Object[]{Integer.valueOf(1234)}));
+    }
+    @Test
+    public void testFormatDecimal() {
+        Assert.assertEquals("1.234,56", new MessageFormat("{0,number}", Locale.GERMANY).format(new Object[]{new BigDecimal("1234.56")}));
+    }
+
+    @Test
+    public void testFormatDecimalNoGrouping() {
+        Assert.assertEquals("1234,56", new MessageFormat("{0,number,#.00}", Locale.GERMANY).format(new Object[]{new BigDecimal("1234.56")}));
+    }
+
+}
