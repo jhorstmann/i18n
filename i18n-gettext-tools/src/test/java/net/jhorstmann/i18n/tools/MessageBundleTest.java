ac5c34b52fbaac08d63b2eff88487041b3d92811
Alexander Fischer <fischer.alexander@web.de>
2014-01-23 14:25:36 +0100

    unit-tests extended

--- i18n-gettext-tools/src/test/java/net/jhorstmann/i18n/tools/MessageBundleTest.java
+++ i18n-gettext-tools/src/test/java/net/jhorstmann/i18n/tools/MessageBundleTest.java
@@ -2,6 +2,9 @@ package net.jhorstmann.i18n.tools;
 
 import java.io.File;
 import java.io.IOException;
+
+import org.fedorahosted.tennera.jgettext.HeaderFields;
+import org.fedorahosted.tennera.jgettext.HeaderUtil;
 import org.fedorahosted.tennera.jgettext.Message;
 import org.junit.Assert;
 import org.junit.Test;
@@ -10,7 +13,8 @@ public class MessageBundleTest {
 
     @Test
     public void testHeaderMessageInProperties() throws IOException {
-        MessageBundle bundle = MessageBundle.loadProperties(new File("src/test/resources/net/jhorstmann/i18n/tools/Header.properties"), "net.jhorstmann.i18n.tools.Header", null);
+        MessageBundle bundle = MessageBundle.loadProperties(new File("src/test/resources/net/jhorstmann/i18n/tools/Header.properties"),
+                "net.jhorstmann.i18n.tools.Header", null);
         Message header = bundle.getHeaderMessage();
         Assert.assertNotNull(header);
         Assert.assertEquals("", header.getMsgid());
@@ -19,4 +23,32 @@ public class MessageBundleTest {
         Assert.assertNotNull(pluralForms);
 
     }
+
+    @Test
+    public void testBundleWithHeaderMessage() {
+        MessageBundle bundle = new MessageBundle();
+        Message header = HeaderUtil.generateDefaultHeader();
+        bundle.addMessage(header);
+
+        Message tstHeader = bundle.getHeaderMessage();
+        Assert.assertNotNull(tstHeader);
+    }
+
+    @Test
+    public void testBundleWithHeaderFields() {
+        MessageBundle bundle = new MessageBundle();
+        Message header = HeaderUtil.generateDefaultHeader();
+        HeaderFields fields = HeaderFields.wrap(header);
+
+        fields.setValue(HeaderFields.KEY_ReportMsgidBugsTo, "a@b.com");
+
+        fields.unwrap(header);
+        bundle.addMessage(header);
+
+        Message tstHeader = bundle.getHeaderMessage();
+        Assert.assertNotNull(tstHeader);
+        fields = HeaderFields.wrap(header);
+        Assert.assertTrue("a@b.com".equals(fields.getValue(HeaderFields.KEY_ReportMsgidBugsTo)));
+    }
+
 }
